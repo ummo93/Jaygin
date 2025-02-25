ed42c92d63e8223a1b92ca8d2ca6fab57aeeb738
Dmitry Luńow <ummo93@gmail.com>
2025-02-25 17:14:39 +0400

    Fixed reflection configuration but anyway raylib jni ffm is not support graalvm native-image 23

--- build/build.bat
+++ build/build.bat
@@ -1,10 +1,8 @@
 cd ..
-SET JAVA_HOME=C:\Program Files\Java\graalvm-jdk-21.0.6+8.1
-SET PATH=%PATH%;C:\Program Files\Java\graalvm-jdk-21.0.6+8.1\bin;C:\Program Files\JetBrains\IntelliJ IDEA 2023.3.4\plugins\maven\lib\maven3\bin
+SET JAVA_HOME=C:\Program Files\Java\graalvm-jdk-23.0.2+7.1
+SET PATH=%PATH%;C:\Program Files\Java\graalvm-jdk-23.0.2+7.1\bin;C:\Program Files\JetBrains\IntelliJ IDEA 2023.3.4\plugins\maven\lib\maven3\bin
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
 mvn clean package ^
 && java -agentlib:native-image-agent=config-output-dir=general/src/main/resources/META-INF/native-image -jar general/target/jaygin.jar ^
 && python update_reflect_config.py ^
-&& cd general/target & native-image --target=windows-amd64 -jar jaygin.jar
-:: or with mvn
-::mvn -Pnative native:compile
+&& cd general/target & mvn -Pnative native:compile
