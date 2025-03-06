2e36c39fc041f6d0f56f25a5ec6fcb627fc83654
Dmitry Luńow <ummo93@gmail.com>
2025-03-06 23:30:50 +0400

    Main class renamed

--- sample-game/src/main/java/com/github/ummo93/Launcher.java
+++ sample-game/src/main/java/com/github/ummo93/Launcher.java
@@ -3,7 +3,7 @@ package com.github.ummo93;
 import com.github.ummo93.config.ModuleConfig;
 import com.github.ummo93.framework.JayginApplication;
 
-public class Application {
+public class Launcher {
     public static void main(String[] args) {
         var game = new JayginApplication(new ModuleConfig());
         game.start();
